84b10181357d04e8ee99ca8ad841696a714d77f7
Casey Mcgrath <caseymcgrath1000@gmail.com>
2024-02-07 11:32:06 -0500

    Start React, Vite & TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PantryList from './components/PantryList.jsx';
+
+function App() {
+  return (
+    <div className="flex flex-col bg-blue-200 border-white">
+      <PantryList />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/PantryList.jsx
@@ -0,0 +1,5 @@
+export default function PantryList() {
+  return (
+    <p className="text-green-500 p-4"> Whats up</p>
+  )
+}
